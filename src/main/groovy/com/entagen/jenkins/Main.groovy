2a54199f738c38c0bf4b56d0e9dd8cbbaa0238b6
Andrei Neculau <andrein@tail-f.com>
2014-04-13 15:15:35 +0200

    printConfig doesn't exit

--- src/main/groovy/com/entagen/jenkins/Main.groovy
+++ src/main/groovy/com/entagen/jenkins/Main.groovy
@@ -44,7 +44,6 @@ class Main {
 
         if (argsMap.printConfig) {
             showConfiguration(argsMap)
-            System.exit(0)
         }
 
         def missingArgs = opts.findAll { shortOpt, optMap ->
