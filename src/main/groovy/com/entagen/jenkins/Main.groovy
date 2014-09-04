d1b20c13b46ee8ee0795721c34872aef28cbdb07
VivekPemawat <vivekpemawat@gmail.com>
2014-09-04 15:20:29 +0530

    Update Main.groovy

--- src/main/groovy/com/entagen/jenkins/Main.groovy
+++ src/main/groovy/com/entagen/jenkins/Main.groovy
@@ -4,10 +4,13 @@ package com.entagen.jenkins
 /*
 Bootstrap class that parses command line arguments, or system properties passed in by jenkins, and starts the jenkins-build-per-branch sync process
  */
+
+//  1)declared a nested map
 class Main {
     public static final Map<String, Map<String, Object>> opts = [
             h: [longOpt: 'help', required: false, args: 0, argName: 'help', description: "Print usage information - gradle flag -Dhelp=true"],
-             test: [longOpt: 'test', required: true, args: 0, argName: 'test', description: "Print usage information - gradle flag -Dtest=<test>"],
+            z: [longOpt: 'test', required: true, args: 1, argName: 'test', description: "test paramter"],
+
             j: [longOpt: 'jenkins-url', required: true, args: 1, argName: 'jenkinsUrl', description: "Jenkins URL - gradle flag -DjenkinsUrl=<jenkinsUrl>"],
             u: [longOpt: 'git-url',  required: true, args: 1, argName: 'gitUrl', description: "Git Repository URL - gradle flag -DgitUrl=<gitUrl>"],
             p: [longOpt: 'job-prefix', required: true, args: 1, argName: 'templateJobPrefix', description: "Template Job Prefix, - gradle flag -DtemplateJobPrefix=<jobPrefix>"],
@@ -16,7 +19,7 @@ class Main {
             c: [longOpt: 'print-config', required: false, args: 0, argName: 'printConfig', description:  "Check configuration - print out settings then exit - gradle flag -DprintConfig=true"],
             d: [longOpt: 'dry-run', required: false, args: 0, argName: 'dryRun', description:  "Dry run, don't actually modify, create, or delete any jobs, just print out what would happen - gradle flag: -DdryRun=true"],
             s: [longOpt: 'start-on-create', required: false, args: 0, argName: 'startOnCreate', description:  "When creating a new job, start it at once."],
-            v: [longOpt: 'no-views', required: false, args: 0, argName: 'noViews', description: "Suppress view creation - gradle flag -DnoViews=true"],
+            v: [longOpt: 'no-views', required: false, args: 0, argName: 'noViewsclear', description: "Suppress view creation - gradle flag -DnoViews=true"],
             k: [longOpt: 'no-delete', required: false, args: 0, argName: 'noDelete', description: "Do not delete (keep) branches and views - gradle flag -DnoDelete=true"],
             f: [longOpt: 'filter-branch-names', required: false, args:  1, argName:  'branchNameRegex', description: "Only branches matching the regex will be accepted - gradle flag: -DbranchNameRegex=<regex>"],
             usr: [longOpt: 'jenkins-user',  required: false, args: 1, argName: 'jenkinsUser', description: "Jenkins username - gradle flag -DjenkinsUser=<jenkinsUser>"],
@@ -24,10 +27,15 @@ class Main {
     ]
 
     public static void main(String[] args) {
+        println "hello hi";
+
+        // test function to create a view
+        // test function to create a job in view1/view2
+
         Map<String, String> argsMap = parseArgs(args)
-        showConfiguration(argsMap)
+       // showConfiguration(argsMap)
         JenkinsJobManager manager = new JenkinsJobManager(argsMap)
-        manager.syncWithRepo()
+        //manager.syncWithRepo()
     }
 
     public static Map<String, String> parseArgs(String[] args) {
@@ -77,6 +85,8 @@ class Main {
         println "==============================================================="
         argsMap.each { k, v -> println " $k: ${formatValue(k, v)}" }
         println "==============================================================="
+
+
     }
 
     public static formatValue(String key, String value) {
