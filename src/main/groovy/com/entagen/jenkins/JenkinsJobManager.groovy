684432ce512f25f87caf468bb9326935ec881d0b
VivekPemawat <vivekpemawat@gmail.com>
2014-09-08 11:01:35 +0530

    Update JenkinsJobManager.groovy

--- src/main/groovy/com/entagen/jenkins/JenkinsJobManager.groovy
+++ src/main/groovy/com/entagen/jenkins/JenkinsJobManager.groovy
@@ -148,7 +148,7 @@ class JenkinsJobManager {
 
     public void testFunction() {
         System.out.println(jenkinsApi.getJobNames("Vivek"));
-        createRepo("nestedtype_git","nested_org2","testrepo1");
+        createRepo("nestedtype_git","nested_org2","testrepo2");
 
        // System.out.println(jenkinsApi.getJobConfig("VivekTestSyncYOURPROJECTGitBranchesWithJenkins"));
         //jenkinsApi.cre
