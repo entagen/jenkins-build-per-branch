dae8d68ed53fea2562ed113c6fd46870539df3fd
VivekPemawat <vivekpemawat@gmail.com>
2014-09-26 13:29:10 +0530

    Update JenkinsJobManager.groovy

--- src/main/groovy/com/entagen/jenkins/JenkinsJobManager.groovy
+++ src/main/groovy/com/entagen/jenkins/JenkinsJobManager.groovy
@@ -247,9 +247,10 @@ class JenkinsJobManager {
                 config = config.replace("GIT_URL", gitUrl);
                 config = config.replace("GIT_URL", gitUrl);
                 config = config.replace("GIT_URL", gitUrl);
-               // if(userProfile!=null)
+                if(userProfile!=null)
                 config = config.replace("UserProfile_value", userProfile);
                 config = config.replace("Maven_CMD_value", mavenCmd);
+                if(userProfile!=null)
                 config = config.replace("UserProfile_value", userProfile);
                 config = config.replace("Maven_CMD_value", mavenCmd);
                 config = config.replace("EmailIds_value", emailId);
