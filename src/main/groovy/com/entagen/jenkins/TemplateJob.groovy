7b4753b636ebe51a15edaf292397a65f4b2c11cf
Doron Goffer <goffer@getnexar.com>
2015-11-17 12:40:03 +0200

    removing all non alpha numeric characters from branch names otherwise the build fails

--- src/main/groovy/com/entagen/jenkins/TemplateJob.groovy
+++ src/main/groovy/com/entagen/jenkins/TemplateJob.groovy
@@ -7,7 +7,7 @@ class TemplateJob {
 
     String jobNameForBranch(String branchName) {
         // git branches often have a forward slash in them, but they make jenkins cranky, turn it into an underscore
-        String safeBranchName = branchName.replaceAll('/', '_')
+        String safeBranchName = branchName.replaceAll("[^a-zA-Z0-9_]", "_")
         return "$baseJobName-$safeBranchName"
     }
     
