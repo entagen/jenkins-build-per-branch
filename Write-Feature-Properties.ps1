b7db064d3b553546b53a7e1dad682957e74059ef
Bryan Roscoe <bryan@rev.com>
2016-03-22 16:11:39 -0500

    FOX-6280:Change feature branches to - not _
    FOX-6280:Remove feature prefix from this script

--- Write-Feature-Properties.ps1
+++ Write-Feature-Properties.ps1
@@ -14,7 +14,7 @@ param(
 
 $branches = git ls-remote --heads $GitUrl
 if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
-$features = $branches | ? { $_ -match "\s+refs/heads/feature/(.*)$" } | % { "feature_" + $matches[1] } 
+$features = $branches | ? { $_ -match "\s+refs/heads/feature/(.*)$" } | % { $matches[1] } 
 $features = $features -Join ','
 #use ASCII to avoid Out-File writing a Byte Order Mark
 echo Features=$features | Out-File -Encoding ascii $OutFile
